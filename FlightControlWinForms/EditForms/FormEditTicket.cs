8b6023d3d24fbdeae9e33d690a6f41864ca2581e
Natko <nbiscan7@gmail.com>
2018-02-04 11:09:52 +0100

    ticket support

--- FlightControlWinForms/EditForms/FormEditTicket.cs
+++ FlightControlWinForms/EditForms/FormEditTicket.cs
@@ -36,13 +36,71 @@ namespace FlightControlWinForms
                 lvi = new ListViewItem(tmpPlane);
 
                 listView1.Items.Add(lvi);
-                comboBox2.Items.Add(p.PassengerId.ToString());
-                comboBox1.Items.Add(p.SeatId.ToString());
-                comboBox3.Items.Add(p.StoreId.ToString());
-                comboBox4.Items.Add(tmpPlane[5]);
+                //comboBox2.Items.Add(p.PassengerId.ToString());
+                //comboBox1.Items.Add(p.SeatId.ToString());
+                //comboBox3.Items.Add(p.StoreId.ToString());
+                //comboBox4.Items.Add(tmpPlane[5]);
             }
 
+            //passengers
+            Passenger[] pilots = Program.MyConnection.Passenger.GetAll().ToArray();
 
+            string[] tmpPassenger = new string[5];
+            //ListViewItem lvi;
+
+            foreach (Passenger p in pilots)
+            {
+                tmpPassenger[0] = p.Id.ToString();
+                tmpPassenger[1] = p.Name;
+
+                lvi = new ListViewItem(tmpPassenger);
+
+                comboBox2.Items.Add(tmpPassenger[0] + ' ' + tmpPassenger[1]);
+            }
+
+            //Seat
+            Seat[] seats = Program.MyConnection.Seat.GetAll().ToArray();
+
+            string[] tmpSeat = new string[8];
+
+            foreach (Seat p in seats)
+            {
+                tmpSeat[0] = p.Id.ToString();
+                tmpSeat[1] = p.Num.ToString();
+
+
+                comboBox1.Items.Add(tmpSeat[0] + ' ' + tmpSeat[1]);
+            }
+
+            //Stores
+
+            Store[] stores = Program.MyConnection.Store.GetAll().ToArray();
+
+            string[] tmpStore = new string[8];
+
+            foreach (Store p in stores)
+            {
+                tmpStore[0] = p.Id.ToString();
+                tmpStore[1] = p.Name;
+
+
+                comboBox3.Items.Add(tmpStore[0] + ' ' + tmpStore[1]);
+            }
+
+            //flights
+            Flight[] items = Program.MyConnection.Flight.GetAll().ToArray();
+
+            string[] tmpFlight = new string[8];
+
+            foreach (Flight p in items)
+            {
+                tmpFlight[0] = p.Id.ToString();
+                tmpFlight[1] = p.RouteId.ToString();
+
+
+                comboBox4.Items.Add(tmpFlight[0] + ' ' + tmpFlight[1]);
+
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -56,10 +114,10 @@ namespace FlightControlWinForms
             {
                 Ticket t = Program.MyConnection.Ticket.Get(Convert.ToInt16(listView1.SelectedItems[0].Text));
                 numericUpDown1.Value = t.Price;
-                comboBox1.SelectedIndex = listView1.SelectedIndices[0];
-                comboBox2.SelectedIndex = listView1.SelectedIndices[0];
-                comboBox3.SelectedIndex = listView1.SelectedIndices[0];
-                comboBox4.SelectedIndex = listView1.SelectedIndices[0];
+                comboBox1.SelectedItem = t.SeatId.ToString() + ' ' + Program.MyConnection.Seat.Get(Convert.ToInt16(t.SeatId)).Num;
+                comboBox2.SelectedItem = t.PassengerId.ToString() + ' ' + Program.MyConnection.Passenger.Get(Convert.ToInt16(t.PassengerId)).Name;
+                comboBox3.SelectedItem = t.StoreId.ToString() + ' ' + Program.MyConnection.Store.Get(Convert.ToInt16(t.StoreId)).Name;
+                comboBox4.SelectedItem = t.FlightId.ToString() + ' ' + Program.MyConnection.Flight.Get(Convert.ToInt16(t.FlightId)).RouteId;
 
             }
         }
